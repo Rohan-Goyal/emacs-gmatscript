25f71bc5974c45696697b8194be91c014cec37aa
Rohan Goyal <goyal.rohan.03@gmail.com>
2020-07-28 12:02:27 +0400

    Removed commented-out code.

--- gmatscript.el
+++ gmatscript.el
@@ -90,7 +90,7 @@
              (objects-regex (regexp-opt objects 'words))
              (functions-regex (regexp-opt functions 'words))
              (misc-regex (regexp-opt misc 'words))
-             )                   ;TODO: Strings, comments
+             )                   ; Generates regexes to match these wordlists
         `(
           (,flow-control-regex . font-lock-keyword-face)
           (,flow-ends-regex . font-lock-keyword-face)
@@ -103,74 +103,10 @@
 
         ;; set/modify each char's class
         (modify-syntax-entry ?% "<" synTable)
-        (modify-syntax-entry ?\n ">" synTable)
+        (modify-syntax-entry ?\n ">" synTable) ; Modifies the comment syntax
                                         ;(modify-syntax-entry ?\' (string ?') synTable)
-        ;; more lines here ...
-        ;; return it
         synTable))
 
-(defun gmat-indent-line ()
-  "Indent current line as WPDL code."
-  (interactive)
-  (beginning-of-line)
-  (if (bobp)
-	  (indent-line-to 0)		   ; First line is always non-indented
-	(let ((not-indented t) cur-indent)
-	  (if (looking-at "^[ \t]*End.*") ; If the line we are looking at is the end of a block, then decrease the indentation
-		  (progn
-			(save-excursion
-			  (forward-line -1)
-			  (setq cur-indent (- (current-indentation) default-tab-width)))
-			(if (< cur-indent 0) ; We can't indent past the left margin
-				(setq cur-indent 0)))
-		(save-excursion
-		  (while not-indented ; Iterate backwards until we find an indentation hint
-			(forward-line -1)
-			(if (looking-at "^[ \t]*End.*") ; This hint indicates that we need to indent at the level of the END_ token
-				(progn
-				  (setq cur-indent (current-indentation))
-				  (setq not-indented nil))
-			  (if (looking-at "^[\t]*\\(BeginScript\\|Else\\|For\\|If\\|Optimize\\|Target\\|While\\)\\>") ; This hint indicates that we need to indent an extra level
-				  (progn
-					(setq cur-indent (+ (current-indentation) default-tab-width)) ; Do the actual indenting
-					(setq not-indented nil))
-				(if (bobp)
-					(setq not-indented nil)))))))
-	  (if cur-indent
-		  (indent-line-to cur-indent)
-		(indent-line-to 0))))) ; If we didn't see an indentation hint, then allow no indentation
-
-;; (defun gmat-indent-line ()
-;;   (interactive)
-;;   (beginning-of-line)
-;;   (if (bobp)  ; Check for rule 1
-;;       (indent-line-to 0)
-;;     )
-;;   (let ((not-indented t) cur-indent)
-;;     (if (looking-at "^[ \t]*End.*;") ; Check for rule 2 ;
-;;         (progn
-;;           (save-excursion
-;;             (forward-line -1)
-;;             (setq cur-indent (- (current-indentation) default-tab-width)))
-;;           (if (< cur-indent 0)
-;;               (setq cur-indent 0)))
-;;       (save-excursion
-;;         (while not-indented
-;;           (forward-line -1)
-;;           (if (looking-at "^[ \t]*End.*;") ; Check for rule 3
-;;               (progn
-;;                 (setq cur-indent (current-indentation))
-;;                 (setq not-indented nil))
-;;                                         ; Check for rule 4
-;;             (if (looking-at "\\<\\(BeginScript\\|Else\\|For\\|If\\|Optimize\\|Target\\|While\\)\\>"
-;;                 (progn
-;;                   (setq cur-indent (+ (current-indentation) default-tab-width))
-;;                   (setq not-indented nil))
-;;               (if (bobp) ; Check for rule 5
-;;                   (setq not-indented nil)))))))
-;;     (if cur-indent
-;;         (indent-line-to cur-indent)
-;;       (indent-line-to 0)))) ; If we didn't see an indentation hint, then allow no indentation
 
 ;;;###autoload
           (define-derived-mode gmat-mode js-jsx-mode "gmat mode"
@@ -182,6 +118,4 @@
 
 ;;;###autoload
           (add-to-list 'auto-mode-alist '("\\.script\\'" . gmat-mode))
-
-
           (provide 'gmat-mode)
